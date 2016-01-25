651d2d062145e10649bb42b2a64eb7c15e2394ca
apsteinmetz <apsteinmetz@yahoo.com>
2016-01-25 17:39:17 -0500

    more parsing experiments

--- gettranscript.R
+++ gettranscript.R
@@ -1,7 +1,23 @@
+#install.packages("devtools")
+#require(devtools)
+#install_url("http://www.omegahat.org/Rstem/Rstem_0.4-1.tar.gz")
+#install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.1.tar.gz")
+#install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.2.tar.gz")
+
 library(rvest)
 library(stringi)
 library(tm)
+library("plyr")
+library("ggplot2")
+library("wordcloud")
+library("RColorBrewer")
+library("SnowballC")
+library(sentiment)
+
+
+
 #test
+
 # This page claims to be in iso-8859-1:
 url <- 'http://www.elections.ca/content.aspx?section=res&dir=cir/list&document=index&lang=e#list'
 elections <- read_html(url)
@@ -22,12 +38,11 @@ html("http://www.sec.gov/litigation/suspensions.shtml") %>%
 tocURL <-"http://www.presidency.ucsb.edu/debates.php"
 debateListRaw<- html(tocURL)
 t <- html_nodes(debateListRaw,"table")[8]
-html_nodes(t,"tr")
-t2<-html_nodes(t,"tr")
 
-movie <- read_html("http://www.imdb.com/title/tt1490017/")
-cast <- html_nodes(movie, "#titleCast span.itemprop")
-html_text(cast)
-html_name(cast)
-html_attrs(cast)
-html_attr(t, "href")
+t2<-html_nodes(t,"table")
+html_nodes(t2,"a") %>%html_attr("href")
+
+xpathSApply(debateListRaw,"//td[@class='docdate']")
+xpathSApply(debateListRaw,"//td[@class='doctext']")
+xpathSApply(debateListRaw,"//span[@class='doctext']")
+
